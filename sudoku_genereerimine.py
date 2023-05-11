6791ece8c3e8504bfc5b1fc73b6213610e3d4ddb
ErkiLaanemae <erkilaanemae@gmail.com>
2023-05-12 00:03:20 +0300

    Minor changes
    Täpitähed eemaldatud, failinime kohandatud, sudoku genereerimine parandatud

---
+++ sudoku_genereerimine.py
@@ -0,0 +1,98 @@
+import random
+
+#---------------------------------------kysi_valja_suurust---------------------------------------#
+# Kasutaja saab valida, kui palju numbreid väljalt eemaldatakse
+def kysi_raskusaste ():
+    while True:
+        sisend = input("Sisesta raskusaste (1-3) või 0 kui soovid juhuslikku raskusastet: ")
+        if sisend.isdigit() and int(sisend) < 5:
+            if (int(sisend) == 0):
+                return random.randint(20, 50)
+            elif (int(sisend) < 5):
+                return int(sisend) * 20 + 15
+        else:
+            print("Viga raskusastme lugemisel!\n")
+#---------------------------------------kysi_valja_suurust---------------------------------------#
+
+#----------------------------------kontrollin_vaartuste_sobivust---------------------------------#
+#sudoku reeglid: ühes reas ja veerus ei tohi sama number korduda
+def kontrollin_vaartuste_sobivust (vali, rida, veerg, vaaartus):
+    for i in range(valja_suurus): 
+        for j in range(valja_suurus):
+            if vali[rida][i] == vaaartus or vali[i][veerg] == vaaartus:
+                return False
+    #väiksemas väljas (3x3) ei tohi samuti numbrid korduda
+    pisivali_suurus = int(valja_suurus ** 0.5)
+    read = (rida // pisivali_suurus) * pisivali_suurus
+    veerud = (veerg // pisivali_suurus) * pisivali_suurus
+    for i in range(read, read + pisivali_suurus):
+        for j in range(veerud, veerud + pisivali_suurus):
+            if vali [i][j] == vaaartus:
+                return False
+    return True   
+#----------------------------------kontrollin_vaartuste_sobivust---------------------------------#
+
+#-----------------------------------------------?------------------------------------------------#
+#def lahenda (vali):
+#    for rida in range(valja_suurus):
+#        for veerg in range(valja_suurus):
+#            if vali[rida][veerg] == 0:
+#                for vaartus in range (1, valja_suurus+1):
+#                    if kontrollin_vaartuste_sobivust (vali, rida, veerg, vaartus):
+#                        vali[rida][veerg] = vaartus
+#                        if lahenda (vali):
+#                            return True
+#                        vali[rida][veerg] = 0
+#                return False
+#    return True
+#-----------------------------------------------?-------------------------------------------------#
+
+#-------------------------------------------genereeri---------------------------------------------#
+def genereeri (vali):
+    for rida in range(valja_suurus):
+        for veerg in range(valja_suurus):
+            if vali[rida][veerg] == 0:
+                for vaaartus in range (1, valja_suurus+1):
+                    vaaartus = random.randint(1,9)
+                    if kontrollin_vaartuste_sobivust (vali, rida, veerg, vaaartus):
+                        vali[rida][veerg] = vaaartus
+                        if genereeri (vali):
+                            return True
+                        vali[rida][veerg] = 0
+                return False
+    return True
+#-------------------------------------------genereeri---------------------------------------------#
+
+#-------------------------------------------tyhjenda----------------------------------------------#
+def tyhjenda(vali, tase):
+    for i in range(tase):
+        rida = random.randint(0, valja_suurus-1)
+        veerg = random.randint(0, valja_suurus-1)
+        vali[rida][veerg] = ' ' 
+#-------------------------------------------tyhjenda----------------------------------------------#
+
+
+
+############################################## MAIN ###############################################
+def main ():
+    global valja_suurus
+    # Küsi kasutajalt raskusastet
+    tase = int(kysi_raskusaste())
+    valja_suurus = 9 
+    # Loo tühi mänguväli
+    vali = [[0 for _ in range(valja_suurus)] for _ in range(valja_suurus)]
+    # Täida numbritega
+    genereeri(vali)
+    
+    lahendus = vali# et oleks hiljem lihtsam lahendust näidata
+
+    # Eemalda numbreid vastavalt raskusastmele
+    tyhjenda(vali, tase)
+############################################## MAIN ###############################################
+    
+    for read in vali: #prinditakse(praegu töö testimiseks)
+        for vaartus in read:
+            print (vaartus, end= ' ')
+        print()
+
+main()
